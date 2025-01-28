b7258638980592ce68e4b2ae9af11558e5d67930
morteza <morteza.t.396@gmail.com>
2025-01-28 22:25:42 +0330

    output dist

--- first/firstone/next.config.js
+++ first/firstone/next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    
+    output: "export",
+    distDir: "dist",
     // Optional: Configure how static exports are generated
     // trailingSlash: true,
-    
+
     // Optional: Customize export behavior
     images: {
-      unoptimized: true // Required for static exports
-    }
-  }
-  
-  module.exports = nextConfig
+        unoptimized: true, // Required for static exports
+    },
+};
+
+module.exports = nextConfig;
